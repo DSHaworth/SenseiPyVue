732c307f9120d8adb1ebff68c3b85b08ab693192
Duane Haworth <duanehaworth@hotmail.com>
2020-11-15 10:39:08 -0600

    Implementing VueMaterial

--- client/src/main.js
+++ client/src/main.js
@@ -5,13 +5,12 @@ import store from './store'
 
 import './styles/custom.scss';
 
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false
-
-Vue.use(MdButton)
 
 new Vue({
   router,
